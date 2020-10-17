bc395068e87d86c99a0def349d97bc00645bbf10
Mohamed Keita <mohkeita13@gmail.com>
2020-10-17 18:10:51 +0200

    implement create and getbyid method

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,17 +1,28 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
+  constructor(
+    private lessonService: LessonService
+  ) {}
   @Query(returns => LessonType)
-  lesson() {
-    return {
-      id: 'asdf',
-      name: 'Physics Class',
-      startDate: (new Date()).toISOString(),
-      endDate: (new Date()).toISOString()
+  lesson(
+    @Args('id') id: string,
+  ) {
+    return this.lessonService.getLesson(id);
+  }
 
-    };
+
+
+  @Mutation(returns => LessonType)
+  createLession(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string
+  ) {
+    return this.lessonService.createLesson(name, startDate, endDate);
   }
 
 }
